a2154578ff80cfef9635406dda787b69c8d0eb3b
AdaTony <3099822406@qq.com>
2017-07-11 17:59:53 +0800

    separation html & js
    separation html & js

--- Src/example.js
+++ Src/example.js
@@ -7,3 +7,24 @@ function showPic( whichPic)
     var description = document.getElementById("description");
     description.firstChild.nodeValue = text;
 }
+
+function prepareGallery()
+{
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i =0; i < links.length; ++i)
+    {
+        links[i].onclick = function ()
+        {
+            showPic(this);
+            return false;
+        }
+    }
+}
+
+
+window.onload = prepareGallery;
